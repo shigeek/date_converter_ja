99113acd5f61a2c14a3a3dda7abd84121d8ce4f2
holasoynaoki <naoki13futsal@gmail.com>
2022-08-20 13:21:39 +0900

    :sparkles:tweak

--- lib/date_converter_ja.rb
+++ lib/date_converter_ja.rb
@@ -5,7 +5,23 @@ require_relative "date_converter_ja/version"
 module DateConverterJa
   class Error < StandardError; end
 
-  def ampm
+  def time_with_ampm
+    self.strftime("#{ampm} %H:%M")
+  end
+
+  def time_without_ampm
+    self.strftime("%H:%M")
+  end
+
+  def date_and_time_with_ampm
+    self.strftime("%Y/%m/%d(%a) #{ampm} %H:%M")
+  end
+
+  def date_and_time_without_ampm
+    self.strftime("%Y/%m/%d(%a) %H:%M")
+  end
+
+  private def ampm
     case self.hour
     when 12
       '昼'
@@ -18,18 +34,6 @@ module DateConverterJa
     else
       raise 'Unpredicted time.'
     end
-  end
-
-  def time_with_ampm
-    self.strftime("#{ampm} %H:%M")
-  end
-
-  def time_without_ampm
-    self.strftime("%H:%M")
-  end
-
-  def date_and_time_with_ampm
-    self.strftime("%Y/%m/%d(%a) #{ampm} %H:%M")
   end
 end
 
